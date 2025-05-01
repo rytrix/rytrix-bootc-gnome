cc9221bc9b6217a786df1fde7fa061ed71979830
Ryan <rmw071102@gmail.com>
2025-05-01 16:46:19 -0400

    added in sway packages

--- build_files/build.sh
+++ build_files/build.sh
@@ -9,8 +9,24 @@ set -ouex pipefail
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
+# Install brave browser
+cat << EOF > /etc/yum.repos.d/brave-browser.repo
+[brave-browser]
+name=Brave Browser
+enabled=1
+autorefresh=1
+baseurl=https://brave-browser-rpm-release.s3.brave.com/x86_64
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
+EOF
+
+
 # this installs a package from fedora repos
-dnf5 install -y tmux 
+dnf5 install -y fish \
+                sway waybar wofi mako slurp  brightnessctl gammastep pulseaudio-utils NetworkManager-tui \
+                brave-browser
+
 
 # Use a COPR Example:
 #
@@ -18,7 +34,3 @@ dnf5 install -y tmux
 # dnf5 -y install package
 # Disable COPRs so they don't end up enabled on the final image:
 # dnf5 -y copr disable ublue-os/staging
-
-#### Example for enabling a System Unit File
-
-systemctl enable podman.socket
